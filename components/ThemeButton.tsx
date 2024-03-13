b3074964ee454760573039b84f57a4b02acb89cb
Md-Asikur <asikurrahaman997@gamil.com>
2024-03-14 00:35:40 +0600

    x

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -17,21 +17,8 @@ console.log({theme:theme==="dark"})
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       {theme === "dark" ? (
-        // <SunIcon className="h-5 w-5 text-orange-600" />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="h-5 w-5 text-orange-600"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 16V4"
-          />
-        </svg>
+        <SunIcon className="h-5 w-5 !text-orange-600" />
+       
       ) : (
         <BsMoon className="h-5 w-5 text-slate-800" />
       )}
