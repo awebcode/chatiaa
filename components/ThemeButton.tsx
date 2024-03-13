f09cecf3c0cb7eb05dc2e8bcb529f5e8dce30eb0
Md-Asikur <asikurrahaman997@gamil.com>
2024-03-14 00:17:48 +0600

    x

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -5,7 +5,7 @@ import { BsMoon, BsSun } from "react-icons/bs";
 
 
 const ThemeButton = () => {
-  const { resolvedTheme, setTheme } = useTheme();
+  const {  setTheme,theme } = useTheme();
 
  
 
@@ -14,9 +14,9 @@ const ThemeButton = () => {
       aria-label="Toggle Dark Mode"
       type="button"
       className="flex items-center justify-center rounded-lg p-2 transition-colors "
-      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {resolvedTheme === "dark" ? (
+      {theme === "dark" ? (
         <BsSun className="h-5 w-5 text-orange-500" />
       ) : (
         <BsMoon className="h-5 w-5 text-slate-800" />
