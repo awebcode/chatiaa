dd3a3121ba6b472d838f6be5ff2086e89f0f19cb
Md-Asikur <asikurrahaman997@gamil.com>
2024-03-14 00:54:57 +0600

    c

--- components/ThemeButton.tsx
+++ components/ThemeButton.tsx
@@ -8,20 +8,23 @@ import { BsMoon } from "react-icons/bs";
 const ThemeButton = () => {
   const {  setTheme,theme,resolvedTheme } = useTheme();
 
-console.log({theme:theme==="dark"})
   return (
-    <button
-      aria-label="Toggle Dark Mode"
-      type="button"
-      className="flex items-center justify-center rounded-lg p-2 transition-colors dark:!bg-gray-700 dark:!text-orange-500 "
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-    >
-      {theme == "dark" ? (
-        <SunIcon style={{ color: "#eab308" }} className="h-5 w-5  !text-orange-600" />
-      ) : (
-        <BsMoon className="h-5 w-5 text-slate-800" />
+    <>
+      {theme && (
+        <button
+          aria-label="Toggle Dark Mode"
+          type="button"
+          className="flex items-center justify-center rounded-lg p-2 transition-colors dark:!bg-gray-700 dark:!text-orange-500 "
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          {theme == "dark" ? (
+            <SunIcon style={{ color: "#eab308" }} className="h-5 w-5  !text-orange-600" />
+          ) : (
+            <BsMoon className="h-5 w-5 text-slate-800" />
+          )}
+        </button>
       )}
-    </button>
+    </>
   );
 };
 
