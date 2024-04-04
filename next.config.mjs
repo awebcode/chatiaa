0a2ac1d2a39852b47aebd5a6db6528815438e2ed
Md-Asikur <asikurrahaman997@gamil.com>
2024-04-04 22:46:12 +0600

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 import nextIntl from "next-intl/plugin";
 const withNextIntl = nextIntl();
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;`
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -30,30 +20,17 @@ const nextConfig = {
       use: "raw-loader",
     });
     config.module.rules.push({
-      test: /\.(mp3)$/,
-      use: {
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
-          publicPath: "/_next/static/sounds/",
-          outputPath: "static/sounds/",
-        },
-      },
-    });
+       test: /\.(mp3)$/,
+       use: {
+         loader: "file-loader",
+         options: {
+           name: "[name].[ext]",
+           publicPath: "/_next/static/sounds/",
+           outputPath: "static/sounds/",
+         },
+       },
+     });
     return config;
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
   },
 };
 
