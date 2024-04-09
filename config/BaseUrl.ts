c933fe3e8d8d37fe679c7685d42c5ce7578e5f0f
Md-Asikur <asikurrahaman997@gamil.com>
2024-04-09 20:00:01 +0600

    ok

--- config/BaseUrl.ts
+++ config/BaseUrl.ts
@@ -1,4 +1,4 @@
 export const BaseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://chat-final-xx1.onrender.com/api/v1"
+    ? "https://chatiaa-server.onrender.com/api/v1"
     : "http://localhost:5000/api/v1";
