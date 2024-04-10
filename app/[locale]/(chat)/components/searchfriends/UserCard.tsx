aaa343f02c3d30bd6b3642f6acebe0fbeb2cd2e3
Md-Asikur <asikurrahaman997@gamil.com>
2024-04-10 19:14:23 +0600

    ok

--- app/[locale]/(chat)/components/searchfriends/UserCard.tsx
+++ app/[locale]/(chat)/components/searchfriends/UserCard.tsx
@@ -3,7 +3,17 @@ import React from "react";
 import moment from "moment";
 import { Tuser } from "@/store/types";
 import { useAccessChatMutation } from "../mutations/Chatmutations";
-
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Link } from "@/navigation";
+import { BsThreeDots } from "react-icons/bs";
+import { Button } from "@/components/ui/button";
 const UserCard: React.FC<{ user: Tuser }> = ({ user }) => {
   const mutaion = useAccessChatMutation("closeSearchUsersSheet");
 
@@ -12,31 +22,48 @@ const UserCard: React.FC<{ user: Tuser }> = ({ user }) => {
   };
 
   return (
-    <div
-      onClick={handleClick}
-      className="p-3 rounded-md  cursor-pointer bg-gray-100 hover:bg-gray-200 dark:bg-gray-800  dark:hover:bg-gray-900 duration-300"
-    >
-      <div className="flex items-center gap-2">
-        <div className="h-8 md:h-8 w-8 md:w-8 relative rounded-full  ">
-         
+    <div className="flex items-center justify-between bg-gray-100 hover:bg-gray-200 dark:bg-gray-800  dark:hover:bg-neutral-800 duration-300 rounded-md">
+      <div
+        onClick={handleClick}
+        className="p-3 rounded-md basis-[90%] w-full  cursor-pointer "
+      >
+        <div className="flex items-center gap-2">
+          <div className="h-8 md:h-8 w-8 md:w-8 relative rounded-full  ">
             <Image
               height={35}
               width={35}
               className=" h-full w-full object-cover rounded-full"
               alt={user.name}
-            src={user.image}
-            loading="lazy"
+              src={user.image}
+              loading="lazy"
             />
-        </div>
-        <div className="flex flex-col text-left gap-1">
-          <h3 className="text-xs md:text-sm font-bold">{user.name}</h3>
-          <span className="text-[10px] md:text-xs">{user.email.slice(0, 30)}</span>
-          <span className="text-[10px] md:text-xs">
-            <span className="mr-1"> Joined</span>
-            {moment((user as any).createdAt).fromNow()}
-          </span>
+          </div>
+          <div className="flex flex-col text-left gap-1">
+            <h3 className="text-xs md:text-sm font-bold">{user.name}</h3>
+            <span className="text-[10px] md:text-xs">{user.email.slice(0, 30)}</span>
+            <span className="text-[10px] md:text-xs">
+              <span className="mr-1"> Joined</span>
+              {moment((user as any).createdAt).fromNow()}
+            </span>
+          </div>
         </div>
       </div>
+      {/* Dropdown here */}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size={"icon"} className="bg-transparent">
+            <BsThreeDots className="text-sm md:text-lg" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Menus</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem>
+            <Link href={`/user/profile/${user?._id}`}>View profile</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
