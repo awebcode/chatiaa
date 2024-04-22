c19525bcdd6f4c20c7d663bf248f2686766dd75c
Md-Asikur <asikurrahaman997@gamil.com>
2024-04-22 14:27:33 +0600

    d

--- app/[locale]/(chat)/components/Main.tsx
+++ app/[locale]/(chat)/components/Main.tsx
@@ -40,7 +40,7 @@ const MainClientWrapper = ({ children }: { children: ReactNode }) => {
     if (!selectedChat||!roomId) return router.replace("/chat");
    }, [roomId, router,selectedChat]);
   //<EmptyChat />;
-  if (!selectedChat) return router.replace("/chat");
+  if (!selectedChat) return <EmptyChat />;
   return (
     <div className="border-l border-l-gray-200 dark:border-l-gray-700  w-full  ">
       {/* chat header */}
