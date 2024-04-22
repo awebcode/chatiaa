a132c9b5df644fc6a2113940a26e00a9dbc4d4b0
Md-Asikur <asikurrahaman997@gamil.com>
2024-04-23 00:29:31 +0600

    d

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 export const maxDuration = 10; // 5 seconds
 export const dynamic = "force-dynamic";
+export const runtime="edge"
 import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
 
