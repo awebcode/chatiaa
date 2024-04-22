c78472783405df1058eb7c1f52c145563ea46507
Md-Asikur <asikurrahaman997@gamil.com>
2024-04-22 19:11:02 +0600

    clsx

--- app/[locale]/(chat)/components/messages/typeMessages/ImageMessage.tsx
+++ app/[locale]/(chat)/components/messages/typeMessages/ImageMessage.tsx
@@ -37,9 +37,7 @@ function ImageMessage({
 }) {
   const { selectedChat: currentChatUser, user: currentUser } = useMessageState();
   return (
-    <div
-      className="flex items-center gap-2 max-w-sm md:max-w-2xl"
-    >
+    <div className="flex items-center gap-2 max-w-sm md:max-w-2xl">
       {/* Remove/Replay/Emoji */}
       <RREsystem message={message} isCurrentUserMessage={isCurrentUserMessage} />
       <div className="">
@@ -47,8 +45,8 @@ function ImageMessage({
         <div
           className={`m-4 p-1 rounded-lg ${
             message?.sender?._id === currentChatUser?.userInfo?._id
-              ? "bg-gray-200 text-gray-900 dark:text-gray-200 dark:bg-incoming-background rounded-bl-3xl"
-              : "dark:bg-outgoing-background rounded-br-3xl bg-gray-300"
+              ? "bg-gray-200 text-gray-900 dark:text-gray-200 dark:bg-incoming-background rounded-tr-xl"
+              : "dark:bg-outgoing-background rounded-br-xl bg-gray-300"
           }`}
         >
           <div className={""}>
@@ -57,14 +55,16 @@ function ImageMessage({
               <div className="pt-5">
                 <RepliedMessage message={message} currentUser={currentUser as any} />
               </div>
-              <Image
-                src={`${message.file.url}`}
-                alt={"Unknown file"}
-                className={"rounded-lg w-auto h-auto "}
-                height={300}
-                width={300}
-                loading="lazy"
-              />
+              <div className="w-52 h-52 md:w-80 md:h-80">
+                <Image
+                  src={`${message.file.url}`}
+                  alt={"Unknown file"}
+                  className={"rounded-lg w-auto h-auto "}
+                  height={300}
+                  width={300}
+                  loading="lazy"
+                />
+              </div>
               <FullScreenPreview file={{ url: message?.file?.url, type: message.type }} />
               <RiDownloadCloudFill
                 className="absolute bottom-1 right-1 text-xl cursor-pointer text-gray-300"
