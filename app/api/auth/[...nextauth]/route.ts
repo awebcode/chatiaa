87f9d3b29fbe3f41ef8fc3d446a1266d3d5bcfec
Code with Asikur <141431875+awebcode@users.noreply.github.com>
2024-04-22 21:00:47 +0600

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 export const maxDuration = 10; // 5 seconds
 export const dynamic = "force-dynamic";
-// export const runtime = "edge";
+export const runtime = process.env.NODE_ENV === "production" ? "edge" : "node";
 import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
 
