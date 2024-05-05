8444c731e80fec275398d81cc79f713bb3c6b70e
Md-Asikur <asikurrahaman997@gamil.com>
2024-05-05 22:44:59 +0600

    ok

--- context/MessageContext.tsx
+++ context/MessageContext.tsx
@@ -1,41 +1,25 @@
 // MessageContext.tsx
 "use client";
-import React, { useReducer, ReactNode, useEffect, useMemo } from "react";
+import React, { useReducer, ReactNode } from "react";
 import {
   MessageDispatchContext,
   MessageStateContext,
   messageReducer,
 } from "./reducers/messageReducer";
-import { IChat, State } from "./reducers/interfaces";
+import { State } from "./reducers/interfaces";
 import { useContextSelector } from "use-context-selector";
+import { getDecryptedSelectedChat } from "@/config/EncDecrypt";
 
 // Initialize initialChats as an array of IChat
-let initialChats: IChat[] = [];
-
-// Check if window is defined (in case of SSR) and session storage is supported
-if (typeof window !== "undefined") {
-  const storedChats = localStorage.getItem("chats");
-
-  // Check if storedChats is not null and has at least one chat item with an _id property
-  if (storedChats) {
-    const parsedChats = JSON.parse(storedChats);
-    if (Array.isArray(parsedChats) && parsedChats.length > 0) {
-      initialChats = parsedChats;
-    }
-  }
-}
 
 const initialState: State = {
-  selectedChat:
-    typeof window !== "undefined" && window.localStorage
-      ? JSON.parse(localStorage.getItem("selectedChat") || "null")
-      : null,
+  selectedChat: getDecryptedSelectedChat(process.env.NEXT_PUBLIC_CRYPTO_DATA_SECRET!),
   isSelectedChat: null,
   messages: [],
   user: null,
   totalMessagesCount: 0,
   totalChats: 0,
-  chats: [], //initialChats.length > 0 ? initialChats : 
+  chats: [], //initialChats.length > 0 ? initialChats :
   callInfo: null,
 };
 
