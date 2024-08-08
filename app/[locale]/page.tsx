4a1dc43563ca0aa1371ec1712545d34bc099e281
Code with Asikur <141431875+awebcode@users.noreply.github.com>
2024-08-08 13:22:02 +0600

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/Home";
 import dynamic from "next/dynamic";
 
 // const Home = dynamic(() => import("../../components/Home"),{ssr:false});
-
+export const maxDuration=300
 export default async function MainPage() {
   return (
     <div className="">
